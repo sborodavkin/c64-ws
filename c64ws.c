349e8eb2da79c9ba70dbd4402ad426e8e0c630f2
Sergey Borodavkin <lw0000@Inbox.ru>
2017-04-05 22:06:06 +0200

    Create c64ws.c

---
+++ c64ws.c
@@ -0,0 +1,395 @@
+#include <c64.h>
+#include <cbm.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+////////////////////////////////////////////////////////////////////////////////
+// GLOBALS
+////////////////////////////////////////////////////////////////////////////////
+
+#define poke(A,X)      (*(unsigned char *)A) = (X)
+#define peek(A)        (*(unsigned char *)A)
+#define poke_word(A,X) (*(unsigned int *)A) = (X)
+#define peek_word(A)   (*(unsigned int *)A)
+
+// Explicitly specify RS232 baud rate and mode as byte array instead of string,
+// as CC65 auto-translates strings from ASCII to PETSCII.
+const unsigned char NAME300[] = {0x06, 0x00, 0x00};
+
+// RS232 device number.
+const unsigned int RS232_HANDLER = 2;
+
+// The RS-232 driver expects to have 256-byte buffers for input and output.
+char RS232_READ_BUF[512];
+char RS232_WRITE_BUF[512];
+
+// These memory locations store pointers to the RS-232 read
+// and write buffers. We=E2=80=99ll fill them in with pointers to
+// our read and write buffers before we open the RS-232
+// device. That=E2=80=99ll prevent the KERNAL open routine from
+// allocating buffers from the top of BASIC memory, which
+// would probably really mess us up.
+char** RIBUF = (char**)0x00f7;
+char** ROBUF = (char**)0x00f9;
+
+// GETIN - get a character from default input device, missing in cbm.h
+unsigned char __fastcall__ (*cbm_k_getin)(void) = 0xffe4;
+
+// Request-related defs.
+
+// HTTP request buffer.
+#define MAX_REQUEST_SIZE 4096
+static char REQUEST_BUF[MAX_REQUEST_SIZE];
+
+static char REQUEST_METHOD_GET = 1;
+static char REQUEST_METHOD_POST = 2;
+
+const char PARSE_RESULT_OK = 0;
+const char PARSE_RESULT_WRONG_METHOD = 1;
+const char PARSE_RESULT_NO_URL = 2;
+const char PARSE_RESULT_NO_LINE = 3;
+
+struct http_request {
+  char method;
+  char url[255];
+};
+
+static char HTTP_REQUEST_FIRST_LINE[255];
+static struct http_request PARSED_REQUEST;
+
+// Response-related defs.
+
+const char* HTTP_RESPONSE_CODE_200 = "200 ok";
+const char* HTTP_RESPONSE_CODE_400 = "400 bad request";
+const char* HTTP_RESPONSE_CODE_404 = "404 not found";
+const char* HTTP_RESPONSE_CODE_500 = "500 server error";
+
+// When something goes wrong better hardcode this.
+const char* HTTP_RESPONSE_SERVER_ERROR = "http/1.1 500 server error\n\n";
+
+
+#define MAX_RESPONSE_SIZE 2048
+static char HTTP_RESPONSE[MAX_RESPONSE_SIZE];
+
+const char RESPONSE_OK = 0;
+const char RESPONSE_TOO_LONG_FILENAME = 1;
+const char RESPONSE_NOT_FOUND = 2;
+const char RESPONSE_FILE_OPEN_PROBLEM = 3;
+const char RESPONSE_FILE_READ_PROBLEM = 4;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Converts the input_str to lowercase (replacing the original).
+char* to_lower(char* input_str) {
+  int i = 0;
+  while(input_str[i]) {
+    input_str[i] = tolower(input_str[i]);
+    i++;
+  }
+  return input_str;
+}
+
+// Prints the intro text.
+void print_intro() {
+  int i;
+  for (i=0; i<20; i++) {
+    printf("\n");
+  }
+  printf("**************************************\n");
+  printf("*        C64-BASED HTTP SERVER       *\n");
+  printf("*    (C) SERGEY BORODAVKIN, 2017.    *\n");
+  printf("**************************************\n\n");
+}
+
+// Opens RS232 and returns 0 if success.
+unsigned int open_rs232() {
+  int res = 0;
+  printf("Opening RS232...\n");
+  // Set up rs-232 buffers.
+  *RIBUF = (char*)(((int)RS232_READ_BUF & 0xff00) + 256);
+  *ROBUF = (char*)(((int)RS232_WRITE_BUF & 0xff00) + 256);
+
+  // Open rs232 channel.
+  cbm_k_setlfs(RS232_HANDLER, 2, 3);
+  cbm_k_setnam (NAME300);
+  res = cbm_k_open();
+  cbm_k_chkin(RS232_HANDLER);
+  return res;
+}
+
+// Check if buffer ends with an empty line (either \n\n or \r\n\r\n).
+bool _is_request_end(char* buffer, unsigned int size) {
+  if (size >= 4) {
+    // Can be either 2* \n or 2* \r\n.
+    if ((buffer[size-1] == '\n' && buffer[size-2] == '\n') ||
+        (buffer[size-1] == '\n' && buffer[size-3] == '\n')) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (size >= 2) {
+    // Can only be \n\n.
+    return (buffer[size-1] == '\n' && buffer[size-2] == '\n');
+  }
+  return false;
+}
+
+// Parses the HTTP request from char* into struct http_request.
+char _parse_request(char* request, struct http_request* result) {
+  const char *first_line_end;
+  char* space_1_idx;
+  char* space_2_idx;
+
+  first_line_end = strchr(request, '\n');
+  if (first_line_end) {
+    strncpy(HTTP_REQUEST_FIRST_LINE, request, first_line_end-request);
+    to_lower(HTTP_REQUEST_FIRST_LINE);
+    if (HTTP_REQUEST_FIRST_LINE[1] == 'g' &&
+        HTTP_REQUEST_FIRST_LINE[2] == 'e' &&
+        HTTP_REQUEST_FIRST_LINE[3] == 't' &&
+        HTTP_REQUEST_FIRST_LINE[4] == ' ') {
+      // GET request.
+      result->method = REQUEST_METHOD_GET;
+    } else {
+      if (HTTP_REQUEST_FIRST_LINE[1] == 'p' &&
+          HTTP_REQUEST_FIRST_LINE[2] == 'o' &&
+          HTTP_REQUEST_FIRST_LINE[3] == 's' &&
+          HTTP_REQUEST_FIRST_LINE[4] == 't' &&
+          HTTP_REQUEST_FIRST_LINE[5] == ' ') {
+        // POST request.
+        result->method = REQUEST_METHOD_POST;
+      }
+    }
+    if (result->method) {
+      space_1_idx = strchr(HTTP_REQUEST_FIRST_LINE, ' ');
+      if (space_1_idx) {
+        space_2_idx = strchr(space_1_idx+1, ' ');
+        if (space_2_idx) {
+          *space_2_idx = 0; // ignore HTTP version
+          strncpy(result->url, space_1_idx+1,
+(int)(space_2_idx-space_1_idx));
+          return PARSE_RESULT_OK;
+        } else {
+          return PARSE_RESULT_NO_URL;
+        }
+      } else {
+        return PARSE_RESULT_NO_URL;
+      }
+    } else {
+      return PARSE_RESULT_WRONG_METHOD;
+    }
+  }
+  return PARSE_RESULT_NO_LINE;
+}
+
+// Deals with request and creates the response string.
+// Memory for response must be pre-allocated.
+char _create_response(const struct http_request* request, char* response) {
+  /*
+  unsigned int max_filename_length = 255 - 14;
+  int file_size = 0;
+  int res = 0;
+  int i = 0;
+  char c;
+  char file_size_str[4];
+  char* filename = (unsigned char*) malloc(max_filename_length);
+  char* file_data = (unsigned char*) malloc(MAX_RESPONSE_SIZE - 100);
+  if (filename && file_data) {
+    filename = "0:";
+    if (strlen(request->url) == 1) {
+      filename = "0:index.htm";
+    } else {
+      strcat(filename, request->url+1);  // +1 to trim '/'
+    }    
+    strcat(filename, ",s,r");
+    printf("loading file %s", filename);
+    
+    cbm_k_close(2);
+    
+    res = cbm_open(6, 8, CBM_READ, filename);    
+    if (!res) {      
+      file_size = cbm_read(6, file_data, MAX_RESPONSE_SIZE);
+      if (file_size > 0) {
+        response = "";
+        strcat(response, "http/1.1 ");
+        strcat(response, HTTP_RESPONSE_CODE_200);
+        strcat(response, "\r\n");
+        strcat(response, "content-length: ");
+        sprintf(file_size_str, "%d", file_size);
+        strcat(response, file_size_str);
+        strcat(response, "\r\n");
+        strcat(response, file_data);      
+      } else {
+        printf("os_error %d\n", _oserror);
+        response = "";
+        strcat(response, HTTP_RESPONSE_SERVER_ERROR);
+      }
+      cbm_close(6);
+    } else {
+      printf("Error opening file [%s], code=%d\n", filename, res);
+    }
+    free(filename);
+    free(file_data);
+    
+    open_rs232();
+    
+  } else {
+    response = "";
+    strcat(response, HTTP_RESPONSE_SERVER_ERROR);
+  }
+  return 'a';
+  */
+  
+  // 16 (max on C64) + 1 byte for \0 + 1 byte for \0 + 5 for disk ID and mode.
+  char* filename = (char*) malloc(16+1+5);  
+  char* file_data = (unsigned char*) malloc(2048);
+  int file_size = -1;
+  int response_code = RESPONSE_OK;
+  int open_code = 0;
+  char* content_length_str = "    ";  // Helper string to store CL as string.
+
+  response = "http/1.1 ";  
+  if (strlen(request->url) > 17) {
+    strcat(response, HTTP_RESPONSE_CODE_400);
+    strcat(response, "\r\n");
+    response_code = RESPONSE_TOO_LONG_FILENAME;
+  } else {
+    // Check for "GET / HTTP/1.1" - request index.htm by default.
+    if (strlen(request->url) == 1) {
+      filename = "0:index.htm,s,r";
+    } else {
+      sprintf(filename, "0:%s,s,r", request->url+1);  // +1 to trim leading '/'
+    }
+    
+    open_code = cbm_open(6, 8, CBM_READ, filename);
+    if (open_code != 0) {
+      if (open_code == 4) {
+        strcat(response, HTTP_RESPONSE_CODE_404);
+        response_code = RESPONSE_NOT_FOUND;
+      } else {
+        strcat(response, HTTP_RESPONSE_CODE_400);
+        response_code = RESPONSE_FILE_OPEN_PROBLEM;
+      }
+      strcat(response, "\r\n");
+    } else {      
+      file_size = cbm_read(6, file_data, 2048);
+      if (file_size = -1 || !file_size) {
+        // Generic error during file read.
+        strcat(response, HTTP_RESPONSE_CODE_500);
+        response_code = RESPONSE_FILE_READ_PROBLEM;
+        strcat(response, "\r\n");
+      } else {
+        sprintf(content_length_str, "%d", file_size);
+        
+        strcat(response, HTTP_RESPONSE_CODE_200);
+        strcat(response, "\r\n");
+        strcat(response, "content-length: ");        
+        strcat(response, content_length_str);
+        strcat(response, "\r\n");
+        strcat(response, file_data);  
+        strcat(response, "\r\n");        
+      }
+      cbm_close(6);  // Close the file only if it has been opened.
+    }
+  }
+  free(filename);
+  free(file_data);  
+  
+  return response_code;
+}
+
+void _handle_request(char* request, unsigned int size) {
+  unsigned int i = 0;
+  char res;
+  printf("Serving request:\n");
+  for(; i < size; i++) {
+    putchar(request[i]);
+  }
+  res = _parse_request(request, &PARSED_REQUEST);
+  if (res == PARSE_RESULT_OK) {
+    _create_response(&PARSED_REQUEST, HTTP_RESPONSE);
+  } else {
+    printf("Error parsing request, code=%d\n", (int)res);
+  }
+}
+
+// Main loop of the app.
+void main_loop() {
+  unsigned int cur_char_idx = 0;
+  unsigned char c;
+  unsigned int PROGRESS_STEP = 20;  // How often to update the progress bar.
+  bool is_new_request = true;
+  for (;;) {
+    if (cur_char_idx >= MAX_REQUEST_SIZE) {
+      printf ("\n!!! Request too large, throttling !!!\n");
+      cur_char_idx = 0;
+      is_new_request = true;
+    }
+    cbm_k_chkin(RS232_HANDLER);
+    c = cbm_k_getin();
+    if (c) {
+      REQUEST_BUF[cur_char_idx++] = c;
+      if ((cur_char_idx-1) % PROGRESS_STEP == 0) {
+        if (is_new_request) {
+          printf ("\nStart getting request: [");
+          is_new_request = false;
+        }
+        printf (".");
+      }
+      if (c == '\n' && _is_request_end(REQUEST_BUF, cur_char_idx)) {
+        printf ("]\n");
+        REQUEST_BUF[cur_char_idx] = 0;  // Make 0-terminated string.
+        _handle_request(REQUEST_BUF, cur_char_idx);
+        cur_char_idx = 0;
+        is_new_request = true;
+      }
+    }
+  }
+}
+
+int main (void) {
+  unsigned int rs232_open_code;
+
+  // Switch to ALL CAPS (cc65 switches to lowercase in startup code).
+  // poke(0xd018, 0x15);
+
+  print_intro();
+  if ((rs232_open_code = open_rs232()) > 10) {
+    printf("Starting main loop.\n");
+    main_loop();
+  } else {
+    printf("Error opening RS232, code %d", rs232_open_code);
+  }
+
+
+
+  /*
+  for (;;)
+  {
+    // look for a keyboard press
+    // cbm_k_chkin (0);
+    // c = cbm_k_getin();
+    // if (c)
+    //{
+    //putchar(c);
+    //cbm_k_ckout(2);
+    //cbm_k_bsout(c);
+    //}
+
+    // look for input on rs232
+    cbm_k_chkin(2);
+    c = cbm_k_getin ();
+    if (c)
+    {
+      putchar(c);
+    }
+  }
+  */
+  return EXIT_SUCCESS;
+}
